c70994847d4ce1f2356888e2f50011190c1e7f36
Ivonne Cervantes <madameiv.264@gmail.com>
2019-11-07 08:45:33 -0500

    nav responsive

--- src/data.js
+++ src/data.js
@@ -9,3 +9,4 @@ export const traerDataPokemon = (array) => {
   }
   return newArray;
 }
+
--- src/main.js
+++ src/main.js
@@ -23,7 +23,26 @@ for(let i = 0; i < dataModificada.length; i++){
 console.log(stringTemplate);
 Seccioncard.innerHTML = stringTemplate;
 traerDataPokemon(POKEMON);
-
-
 //Backtips y es la nueva forma de concatenar cadenas en es6 ${} interpolar variables
+
+
+//menu bar 
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
 
